66e8a8503256c1491d68387eba2c3fa548f18d5a
Dinko Azema <rocket4ce@gmail.com>
2025-09-02 21:06:20 -0400

    Initialize Elixir project with formatter, gitignore, mix configuration, and basic module structure

---
+++ lib/phx_gen_auth_api.ex
@@ -0,0 +1,18 @@
+defmodule PhxGenAuthApi do
+  @moduledoc """
+  Documentation for `PhxGenAuthApi`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhxGenAuthApi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phx_gen_auth_api_test.exs
@@ -0,0 +1,8 @@
+defmodule PhxGenAuthApiTest do
+  use ExUnit.Case
+  doctest PhxGenAuthApi
+
+  test "greets the world" do
+    assert PhxGenAuthApi.hello() == :world
+  end
+end
